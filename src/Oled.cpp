e6fa61234fc5f70abbd158f1b81759340f8ef466
CaliforniaCinnamon <kimws27@yonsei.ac.kr>
2020-07-20 00:09:54 +0900

    edit OLED class final

--- src/Oled.cpp
+++ src/Oled.cpp
@@ -3,33 +3,63 @@
 // 
 
 #include "Oled.h"
-
 Oled::Oled() {
 	//idisplay(4);
 	idisplay.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
 }
 
-
-void Oled::display_character(const char* msg) {
-	this->clear();
-	uint16_t CURSOR_X = 0, CURSOR_Y = 5;
-	uint8_t TEXTSIZE = 1;
-	idisplay.setCursor(CURSOR_X, CURSOR_Y);  //텍스트를 디스플레이 중간에 표시
-	idisplay.setTextSize(TEXTSIZE); //사이즈 조절
-	idisplay.setTextColor(WHITE); //텍스트 color
-
-	idisplay.print(msg);
+void Oled::display_bluetooth() {
+	idisplay.setTextColor(WHITE);
+	idisplay.setTextSize(1);
+	idisplay.setCursor(30, 27);
+	idisplay.println("Bluetooth");
+	idisplay.setCursor(20, 35);
+	idisplay.println("is connected");
+	idisplay.display();
+	delay(2000);
 }
 
 
-// 그래픽 데이터 출력하는 함수
-void Oled::display_graphic(const uint8_t* graphic_data) {
-	this->clear();
-	// 인자 순서대로: 가로위치, 세로위치, 그래픽데이터, 가로크기, 세로크기, 색상
-	uint16_t pos_x = 32;  uint16_t pos_y = 0; // 위치
-	uint16_t size_x = 64;  uint16_t size_y = 64; // 크기
-	idisplay.drawBitmap(pos_x, pos_y, graphic_data, size_x, size_y, WHITE);
+void Oled::display_complete() {
+	idisplay.setTextColor(WHITE);
+	idisplay.setTextSize(1);
+	idisplay.setCursor(30, 30);
+	idisplay.println("Complete!");
+	idisplay.display();
+	delay(2000);
+}
+
+//이거 여기 안에서 for문 반복시키면 안되던데 왜 안되는건지
+//그래서 어쩔 수 없이 인자 숫자로 받고 main함수에서 for 문 돌리니까 됨
+void Oled::display_process(int i) {
+	idisplay.drawTriangle(5, 5, 55, 5, 30, 35, WHITE);
+	idisplay.drawLine(30, 35, 30, 60, WHITE);
+	idisplay.drawLine(15, 60, 45, 60, WHITE);
+	int d1 = 1.2 * i - 1;
+	int d2 = -1.2 * i + 63;
+	idisplay.drawLine(i, d1, d2, d1, WHITE);
+	idisplay.display();
+	delay(2000);
+	idisplay.clearDisplay();
+	delay(2000);
+}
+
+//칵테일 이름 출력(이름 긴 거는 띄어쓰기 기준으로 줄바꿈)
+void Oled::display_cocktail(char* msg) {
+	idisplay.setTextColor(WHITE);
+	idisplay.setTextSize(1);
+	char* result = strtok(msg, " ");
+	//i 초기 값이랑 증가값 조정 필요
+	int i = 30;
+	while (result != NULL) {
+		idisplay.setCursor(60, i);
+		idisplay.println("%s", result);
+		result = strtok(NULL, " ");
+		i += 10;
+	}
+	idisplay.display();
+	delay(2000);
 }
 
 void Oled::clear() {
@@ -40,13 +70,11 @@ void Oled::show() {
 	idisplay.display();
 }
 
-
+/*
 void Oled::temp() {
-	idisplay.drawPixel(5, 5, 1);
-	idisplay.drawPixel(10, 10, 1);
-	idisplay.drawCircle(32, 32, 5, 1);
-	idisplay.drawChar(20, 3, 'a', 1, 1, 1);
-	idisplay.drawChar(26, 3, 'b', 1, 1, 1);
-}
-
-
+  idisplay.drawPixel(5, 5, 1);
+  idisplay.drawPixel(10, 10, 1);
+  idisplay.drawCircle(32, 32, 5, 1);
+  idisplay.drawChar(20, 3, 'a', 1, 1, 1);
+  idisplay.drawChar(26, 3, 'b', 1, 1, 1);
+}*/
--- src/Oled.h
+++ src/Oled.h
@@ -4,22 +4,27 @@
 #define _OLED_h
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
+#include "arduino.h"
 #else
-	#include "WProgram.h"
+#include "WProgram.h"
 #endif
 
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+#include <SPI.h>
 
 class Oled {
 private:
 	Adafruit_SSD1306 idisplay; // 그래픽 출력용 인스턴스
 public:
 	Oled();
-	void display_character(const char* msg);
-	void display_graphic(const uint8_t* graphic_data);
+	void display_bluetooth();
+	void display_complete();
+	void display_process(int i);
 	void clear();
+	void display_cocktail(char* msg);
 	void show();
-
 	void temp();
 
 };
