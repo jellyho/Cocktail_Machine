578d6eb9c7083d1d35105fbcb48c065773e13cfb
CaliforniaCinnamon <kimws27@yonsei.ac.kr>
2020-07-19 23:17:44 +0900

    add actuator class (inline = header only)

---
+++ Actuator.h
@@ -0,0 +1,48 @@
+// Cocktail.h
+
+#ifndef _ACTUATOR_h
+#define _ACTUATOR_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
+class Actuator
+{
+private:
+    int PIN_A, PIN_B;
+
+public:
+    Actuator(int a, int b);
+    void up();
+    void down();
+    void idle();
+};
+
+
+Actuator::Actuator(int a, int b) {
+    PIN_A = a;
+    PIN_B = b;
+    pinMode(PIN_A, OUTPUT);
+    pinMode(PIN_B, OUTPUT);
+}
+
+void Actuator::up() {
+    digitalWrite(PIN_A, HIGH);
+    digitalWrite(PIN_B, LOW);
+}
+
+void Actuator::down() {
+    digitalWrite(PIN_A, LOW);
+    digitalWrite(PIN_B, HIGH);
+}
+
+void Actuator::idle() {
+    digitalWrite(PIN_A, LOW);
+    digitalWrite(PIN_B, LOW);
+};
+
+#endif
